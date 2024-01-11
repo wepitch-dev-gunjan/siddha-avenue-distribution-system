a7a84d22eafb3a8cbad1f2982cb6ae550550257d
gunjan <gunjan.soral@gmail.com>
2024-01-11 15:31:08 +0530

    ruko

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const { getRole, getRoles, deleteRole, createRole } = require('../controllers/roleController');
 const router = express.Router();
 
-router.get('/role',)
+router.post('/role', createRole);
+router.get('/role/:role_id', getRole);
+router.get('/role', getRoles);
+router.delete('/role', deleteRole);
 
 module.exports = router;
