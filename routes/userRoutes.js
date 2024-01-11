347b76bf5286a27d49fe85f1b4e314c9692c39b6
gunjan <gunjan.soral@gmail.com>
2024-01-11 15:02:06 +0530

    login register done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { getUser, getUsers } = require('../controllers/userController');
+const { getUser, getUsers, login } = require('../controllers/userController');
 const { userAuth, adminAuth } = require('../middlewares/authMiddlewares');
+const { register } = require('module');
 const router = express.Router();
 
 router.get('/user', userAuth, adminAuth, getUsers,);
 router.get('/user/:user_id', userAuth, getUser,);
+router.post('/user/register', register);
+router.get('/user/login', login);
 
 module.exports = router;
