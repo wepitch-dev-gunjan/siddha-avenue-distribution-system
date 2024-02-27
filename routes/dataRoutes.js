582d252e46f80ddbb2a5457e8bfbdb188ccb14a2
Nmn200 <n9462895233@gmail.com>
2024-02-27 12:16:11 +0530

    f

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { uploadData } = require("../controllers/dataController");
 const { upload } = require("../services/fileUpload");
+const { userAuth } = require("../middlewares/authMiddlewares");
 const router = express.Router();
 
-router.post("/upload", upload.single("file"), uploadData);
+router.post("/upload", userAuth, upload.single("file"), uploadData);
 
 module.exports = router;
