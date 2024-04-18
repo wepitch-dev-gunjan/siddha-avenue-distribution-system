4ae5c9acdd4a39000483ad84df0d1c3024749296
gunjan <gunjan.soral@gmail.com>
2024-04-18 11:28:25 +0530

    YTD logic completely done

--- controllers/salesDataController.js
+++ controllers/salesDataController.js
@@ -333,9 +333,130 @@ exports.getSalesData = async (req, res) => {
         // If a matching channel is found, merge LYTD Sell out into the currentChannel object
         if (matchingChannel) {
           currentChannel["LYTD Sell out"] = matchingChannel["LYTD Sell out"];
+          currentChannel["%Gwth"] = parseInt(currentChannel["YTD Sell out"]) / parseInt(matchingChannel["LYTD Sell out"]) * 100;
         } else {
           // If no matching channel is found, set LYTD Sell out to 0
           currentChannel["LYTD Sell out"] = 0;
+          currentChannel["%Gwth"] = 0;
+        }
+      });
+
+      // Now, salesStats array contains LYTD Sell out for each channel
+      if (!salesStats || salesStats.length === 0) return res.status(404).send({ error: "Data not found" });
+      res.status(200).send(salesStats);
+    }
+
+    if (td_format === 'YTD' && data_format === 'volume') {
+      const lastYearSalesStats = await SalesData.aggregate([
+        {
+          $match: {
+            DATE: {
+              $gte: `${startYear - 1}-01-01`, // Start of the previous year
+              $lte: `${endYear - 1}-${endMonth.toString().padStart(2, '0')}-${presentDayOfMonth}` // End of the previous year's current month
+            }
+          }
+        },
+        {
+          $group: {
+            _id: "$CHANNEL",
+            "YTD VOLUME": { $sum: { $toInt: "$MTD VOLUME" } }
+          }
+        },
+        {
+          $group: {
+            _id: null,
+            totalLYTDSellOut: { $sum: "$YTD VOLUME" },
+            channelsData: { $push: "$$ROOT" } // Preserve grouped data for further processing
+          }
+        },
+        {
+          $unwind: "$channelsData"
+        },
+        {
+          $project: {
+            "Channel": "$channelsData._id",
+            "LYTD Sell out": "$channelsData.YTD VOLUME"
+          }
+        }
+      ]);
+
+      const lastDays = getLastDaysOfPreviousMonths()
+      const salesStats = await SalesData.aggregate([
+        {
+          $match: {
+            DATE: {
+              $gte: `${startYear}-01-01`, // Start of the current month
+              $lte: `${endYear}-${endMonth.toString().padStart(2, '0')}-${presentDayOfMonth}` // End of the current month
+            }
+          }
+        },
+        {
+          $group: {
+            _id: "$CHANNEL",
+            "YTD VOLUME": { $sum: { $toInt: "$MTD VOLUME" } }
+          }
+        },
+        // Stage 2: Calculate total MTD Sell out
+        {
+          $group: {
+            _id: null,
+            totalYTDSellOut: { $sum: "$YTD VOLUME" },
+            channelsData: { $push: "$$ROOT" } // Preserve grouped data for further processing
+          }
+        },
+        // Stage 3: Unwind the array to access grouped data
+        {
+          $unwind: "$channelsData"
+        },
+        // Stage 4: Calculate %Gwth (percentage growth) and Contribution
+        {
+          $project: {
+            "Channel": "$channelsData._id",
+            "YTD Sell out": "$channelsData.YTD VOLUME",
+            "%Gwth": {
+              $cond: {
+                if: { $eq: ["$channelsData.LYTD VOLUME", 0] },
+                then: 0,
+                else: {
+                  $multiply: [
+                    { $divide: [{ $subtract: ["$channelsData.YTD VOLUME", "$channelsData.LYTD VOLUME"] }, "$channelsData.LYTD VOLUME"] },
+                    100
+                  ]
+                }
+              }
+            },
+            "Contribution": {
+              $cond: {
+                if: { $eq: ["$totalYTDSellOut", 0] }, // Handling zero totalMTDSellOut
+                then: 0,
+                else: {
+                  $multiply: [
+                    { $divide: ["$channelsData.YTD VOLUME", "$totalYTDSellOut"] },
+                    100
+                  ]
+                }
+              }
+            }
+          }
+        },
+        {
+          $sort: { "Contribution": -1 }
+        }
+      ]);
+
+      // Loop through each element in salesStats array
+      salesStats.forEach(currentChannel => {
+        // Find the corresponding channel in lastYearSalesStats array
+        const matchingChannel = lastYearSalesStats.find(channel => channel.Channel === currentChannel.Channel);
+
+        // If a matching channel is found, merge LYTD Sell out into the currentChannel object
+        if (matchingChannel) {
+          currentChannel["LYTD Sell out"] = matchingChannel["LYTD Sell out"];
+          currentChannel["%Gwth"] = parseInt(currentChannel["YTD Sell out"]) / parseInt(matchingChannel["LYTD Sell out"]) * 100;
+        } else {
+          // If no matching channel is found, set LYTD Sell out to 0
+          currentChannel["LYTD Sell out"] = 0;
+          currentChannel["%Gwth"] = 0;
         }
       });
 
@@ -349,7 +470,3 @@ exports.getSalesData = async (req, res) => {
     return res.status(500).send("Internal Server Error");
   }
 };
-
-
-
-
