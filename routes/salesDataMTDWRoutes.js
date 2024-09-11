9059c8c8bfab968f814ad076853b0195635242ec
rakshitaaa <rakshitakanwar09@gmail.com>
2024-09-11 14:13:20 +0530

    segment wise mtdw

--- routes/salesDataMTDWRoutes.js
+++ routes/salesDataMTDWRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../services/fileUpload");
-const { uploadSalesDataMDTW, getSalesDashboardDataMDTW, getSalesDataChannelWiseForEmployeeMDTW } = require("../controllers/salesDataMTDWController");
+const { uploadSalesDataMTDW, getSalesDashboardDataMTDW, getSalesDataChannelWiseForEmployeeMTDW, getSalesDataSegmentWiseForEmployeeMTDW } = require("../controllers/salesDataMTDWController");
 
-router.post("/sales-data-mtdw", upload.single("file"), uploadSalesDataMDTW);
-router.get("/sales-data-mtdw/dashboard", getSalesDashboardDataMDTW);
-router.get("/sales-data-mtdw/channel-wise/employee", getSalesDataChannelWiseForEmployeeMDTW);
+router.post("/sales-data-mtdw", upload.single("file"), uploadSalesDataMTDW);
+router.get("/sales-data-mtdw/dashboard", getSalesDashboardDataMTDW);
+router.get("/sales-data-mtdw/channel-wise/employee", getSalesDataChannelWiseForEmployeeMTDW);
+router.get("/sales-data-mtdw/segment-wise/employee", getSalesDataSegmentWiseForEmployeeMTDW);
 
 module.exports = router;
