25d15ecbae3cfcd91dc93243a674894543625f02
rakshitaaa <rakshitakanwar09@gmail.com>
2024-09-12 10:59:11 +0530

    dash dealer mtdw

--- routes/salesDataMTDWRoutes.js
+++ routes/salesDataMTDWRoutes.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../services/fileUpload");
-const { uploadSalesDataMTDW, getSalesDashboardDataMTDW, getSalesDataChannelWiseForEmployeeMTDW, getSalesDataSegmentWiseForEmployeeMTDW } = require("../controllers/salesDataMTDWController");
+const { 
+    uploadSalesDataMTDW, 
+    getSalesDashboardDataForEmployeeMTDW, 
+    getSalesDataChannelWiseForEmployeeMTDW, 
+    getSalesDataSegmentWiseForEmployeeMTDW, 
+    getSalesDashboardDataForDealerMTDW
+} = require("../controllers/salesDataMTDWController");
 
 router.post("/sales-data-mtdw", upload.single("file"), uploadSalesDataMTDW);
-router.get("/sales-data-mtdw/dashboard", getSalesDashboardDataMTDW);
+
+// EMPLOYEE ROUTES 
+router.get("/sales-data-mtdw/dashboard/employee", getSalesDashboardDataForEmployeeMTDW);
 router.get("/sales-data-mtdw/channel-wise/employee", getSalesDataChannelWiseForEmployeeMTDW);
 router.get("/sales-data-mtdw/segment-wise/employee", getSalesDataSegmentWiseForEmployeeMTDW);
+
+// DEALER ROUTES 
+router.get("/sales-data-mtdw/dashboard/dealer", getSalesDashboardDataForDealerMTDW);
+
 
 module.exports = router;
