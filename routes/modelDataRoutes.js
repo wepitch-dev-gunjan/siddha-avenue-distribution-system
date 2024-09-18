c645f076f8e6b69394d2ef43bef01550138e198a
rakshitaaa <rakshitakanwar09@gmail.com>
2024-09-18 14:54:43 +0530

    model w for emp

--- routes/modelDataRoutes.js
+++ routes/modelDataRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { upload } = require("../services/fileUpload");
-const { uploadModelData, getSalesDataModelWise } = require("../controllers/modelDataController");
+const { uploadModelData, getSalesDataModelWise, getSalesDataModelWiseForEmployeeMTDW } = require("../controllers/modelDataController");
+const { userAuth } = require("../middlewares/authMiddlewares");
 const router = express.Router();
 
 router.post("/model-data", upload.single("file"), uploadModelData);
 router.get("/model-data", getSalesDataModelWise);
+
+// MTDW 
+router.get("/model-data/mtdw/employee", userAuth, getSalesDataModelWiseForEmployeeMTDW);
 
 module.exports = router;
