1417588263c7da6d2dd17cf5d2ab054f378be56f
rakshitaaa <rakshitakanwar09@gmail.com>
2024-09-19 14:28:43 +0530

    sales channel wise by sbordinate code

--- routes/salesDataMTDWRoutes.js
+++ routes/salesDataMTDWRoutes.js
@@ -9,7 +9,8 @@ const {
     getSalesDashboardDataForDealerMTDW,
     getSalesDataChannelWiseForDealerMTDW,
     getSalesDataSegmentWiseForDealerMTDW,
-    getAllSubordinatesMTDW
+    getAllSubordinatesMTDW,
+    getSalesDataChannelWiseBySubordinateCodeMTDW
 } = require("../controllers/salesDataMTDWController");
 const { dealerAuth, userAuth } = require("../middlewares/authMiddlewares");
 
@@ -19,6 +20,7 @@ router.post("/sales-data-mtdw", upload.single("file"), uploadSalesDataMTDW);
 router.get("/sales-data-mtdw/dashboard/employee", userAuth,  getSalesDashboardDataForEmployeeMTDW);
 router.get("/sales-data-mtdw/channel-wise/employee", userAuth, getSalesDataChannelWiseForEmployeeMTDW);
 router.get("/sales-data-mtdw/segment-wise/employee", userAuth, getSalesDataSegmentWiseForEmployeeMTDW);
+router.get("/sales-data-mtdw/channel-wise/by-subordinate-code/:subordinate_code", getSalesDataChannelWiseBySubordinateCodeMTDW);
 
 // DEALER ROUTES 
 router.get("/sales-data-mtdw/dashboard/dealer", dealerAuth,  getSalesDashboardDataForDealerMTDW);
