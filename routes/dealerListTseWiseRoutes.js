f7f74090d60ec80af2ca97a57cb77b0c2a0d737f
rakshitaaa <rakshitakanwar09@gmail.com>
2024-10-18 12:27:50 +0530

    update dlr lst w seales data

--- routes/dealerListTseWiseRoutes.js
+++ routes/dealerListTseWiseRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../services/fileUpload");
-const { uploadDealerListTseWise } = require("../controllers/dealerListTseWiseController");
+const { uploadDealerListTseWise, updateDealerListWithSalesData } = require("../controllers/dealerListTseWiseController");
 
 router.post("/dealer-list-tse-wise", upload.single("file"), uploadDealerListTseWise);
+router.put("/mapping-update-dealers", updateDealerListWithSalesData);
 
 module.exports = router;
